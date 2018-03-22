d59d84b349395784c605bbd0ddf47fc1c2a29cd3
lain0 <trampampushe4ka@gmail.com>
2018-03-22 20:35:00 +0300

    upgrade -y

--- gcloud_startup-script.sh
+++ gcloud_startup-script.sh
@@ -2,7 +2,7 @@
 # gcloud_startup-script.sh
 
 # install ruby
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y ruby-full ruby-bundler build-essential
 
 # install mondoDB
@@ -10,7 +10,7 @@ sudo apt install -y ruby-full ruby-bundler build-essential
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y mongodb-org
 
 # start MongoDB:
--- install_ruby.sh
+++ install_ruby.sh
@@ -2,6 +2,6 @@
 # install_ruby.sh
 
 # install ruby
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y ruby-full ruby-bundler build-essential
 ruby -v
