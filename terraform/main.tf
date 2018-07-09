e04851df10009ba8ff0ae553f6dd05be99ccf1ab
lain0 <trampampushe4ka@gmail.com>
2018-07-10 02:59:50 +0300

    terraform fmt > main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,11 +38,9 @@ resource "google_compute_instance" "app" {
     source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
-
   provisioner "remote-exec" {
     script = "files/deploy.sh"
   }
-
   # add user/sshkey for provisioner
   connection {
     type        = "ssh"
